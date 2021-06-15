13fa51312b92b3791e9a413cf956e32f9761c0a3
shriya246 <72267340+shriya246@users.noreply.github.com>
2021-06-15 22:08:46 +0530

    Add files via upload

---
+++ Day4.py
@@ -0,0 +1,26 @@
+a=b=c=400
+print('data type of a,b,c :',type(a))
+print('divide a value by 20')
+print(a,'/','20','=',a/20)
+print('multiply b value by 35')
+print(b,'*','35','=',b*35)
+print('add c value by 70')
+print(c,'+','70','=',c+70)
+p='Welcome kh34@'
+print('Sting:',p)
+print('length of the string:',len(p))
+q1='4'
+q2='h'
+c1=p.replace(q1,q2)
+print('New string:',c1)
+a,b=25,10.5
+print('a=',a,'b=',b)
+print('data type of a :',type(a))
+print('data type of b :',type(b))
+a=float(a)
+b=int(b)
+print('value of a and b after type converstion:')
+print('a=',a,'b=',b)
+print('data type of a after type conversion:',type(a))
+print('data type of b after type conversion:',type(b))
+
